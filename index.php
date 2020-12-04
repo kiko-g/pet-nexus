b98b5aafac751439483bf23255c10f48612e0071
kiko-g <kikojpgoncalves@gmail.com>
2020-12-04 12:35:17 +0000

    add templates to pages

--- index.php
+++ index.php
@@ -1,52 +1,11 @@
-<!DOCTYPE html>
-<html lang="en-US">
-
-<head>
-  <title>Pet Nexus</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <!-- Favicons -->
-  <!-- <link href="/assets/img/dog.jpg" rel="icon"/> -->
-  <link href="/assets/img/logo.png" rel="icon"/>
-  
-
-  <!-- Page Icons -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/v4-shims.css">
-  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-
-  <!-- Stylesheets -->
-  <link rel="stylesheet" href="/style/pets.css">
-  <link rel="stylesheet" href="/style/style.css">
-  <link rel="stylesheet" href="/style/fonts.css">
-  <link rel="stylesheet" href="/style/images.css">
-  <link rel="stylesheet" href="/style/topnav.css">
-  <link rel="stylesheet" href="/style/buttons.css">
-  <link rel="stylesheet" href="/style/responsive.css">
-
-  <!-- Scripts -->
-  <script src="/js/topnav.js"></script>
-</head>
-
-
+<?php require 'templates/head.html'; ?>
 <body>
   <header class="header">
     <h1>Pet Nexus</h1>
     <p>A <b>petfinder</b> website</p>
   </header>
 
-  <div class="topnav" id="topnavbar">
-    <a href="/index.php" class="no-border"> <i class="fas fa-home"></i> Home </a>
-    <a href="/pages/profile.php" class=""> <i class="fas fa-user"></i> Profile </a>
-    <a href="/pages/pets.php" class=""> <i class="fas fa-dog"></i> Breeds </a>
-    <a href="/pages/register.php" class="right"> <i class="fas fa-user-plus"></i> Register </a>
-    <a href="/pages/login.php" class="right"> <i class="fa fa-sign-in"></i> Login </a>
-    <a href="#" class="a-search right"> <i class="fa fa-search"> </i> Search </a>
-    <a href="#" class="a-heart right"> <i class="fa fa-heart"> </i> Favorites </a>
-    <a href="javascript:void(0);" class="icon" onclick="topnavResponsive()"><i class="fa fa-bars"></i></a>
-  </div>
+  <?php require 'templates/navbar.php'; ?>
 
   <div class="row">
     <div class="side">
@@ -69,12 +28,7 @@
     </div>
   </div>
 
-  <?php require 'pages/pets.php'; ?>
-
-  <footer>
-    <img src="/assets/img/logo.png" alt="" class="footer-icon">
-    <p>&copy; <strong>Pet Nexus</strong>, LTW 2020</p>
-  </footer>
+  <?php require 'templates/footer.html'; ?>
 </body>
 
 </html>
--- pages/register.php
+++ pages/register.php
@@ -1,39 +1,14 @@
-<!DOCTYPE html>
-<html lang="en-US">
-
-<head>
-  <title>Profile</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <!-- Favicons -->
-  <link href="/assets/img/logo.png" rel="icon"/>
-
-  <!-- Page Icons -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet" >
-  <link href="https://use.fontawesome.com/releases/v5.15.1/css/v4-shims.css" rel="stylesheet" >
-  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-
-  <!-- Stylesheets -->
-  <link rel="stylesheet" href="/style/pets.css">
-  <link rel="stylesheet" href="/style/fonts.css">
-  <link rel="stylesheet" href="/style/style.css">
-  <link rel="stylesheet" href="/style/images.css">
-  <link rel="stylesheet" href="/style/topnav.css">
-  <link rel="stylesheet" href="/style/buttons.css">
-  <link rel="stylesheet" href="/style/responsive.css">
-
-  <!-- Scripts -->
-  <script src="/js/topnav.js"></script>
-</head>
+<?php require '../templates/head.html' ?>
+<?php require '../templates/navbar.php' ?>
 
 <body>
   <article class="row">
-    <section id="page">
+    <section class="page">
       <h1>Register</h1>
     </section>
   </article>
 </body>
+
+<?php require '../templates/footer.html' ?>
 
 </html>
