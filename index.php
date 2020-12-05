37eaaaacec19fd68f62b971b3633f7646a3a90e6
kiko-g <kikojpgoncalves@gmail.com>
2020-12-05 14:33:52 +0000

    redid footer; next up login, profile, signup

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php require 'templates/head.html'; ?>
+
 <body>
   <header class="header">
     <h1>Pet Nexus</h1>
@@ -9,22 +10,12 @@
 
   <div class="row">
     <div class="side">
-      <h2>Pets</h2>
-      <div class="photo">
-        <img src="/assets/img/dog.jpg" alt="dog" style="width: 100%;">
-        <div class="image-container">
-          <p>The cutest boy in town</p>
-        </div>
+      <div class="photo"> 
+        <img src="/assets/img/dog.jpg" alt="dog" class="w100">
       </div>
-      <p>Pets pets pets!</p>
-      <h3>More Pets</h3>
-      <p>Dogs are cool.</p>
     </div>
-    <div class="main">
-      <h2>TITLE1 - DOGS RULE THE WORLD</h2>
-      <h5>Title description</h5>
-      <div class="photo">Image</div>
-      <p>Some text...</p>
+
+    <div class="main">  
     </div>
   </div>
 
---
+++ pages/foundpet.php
@@ -0,0 +1,12 @@
+<?php require '../templates/head.html'; ?>
+<?php require '../templates/navbar.php'; ?>
+<body>
+  <article class="row">
+    <section class="page">
+      <h1>I have a pet for adoption</h1>
+    </section>
+  </article>
+</body>
+<?php require '../templates/footer.html'; ?>
+
+</html>
