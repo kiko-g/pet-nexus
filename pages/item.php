a9806ecd5049f0026da148563a0d777bead98586
kiko-g <kikojpgoncalves@gmail.com>
2020-12-17 13:45:49 +0000

    item.php styling progress checkpoint

--- pages/item.php
+++ pages/item.php
@@ -23,10 +23,10 @@
 
 		<div class="main70">
 			<h2><?= $dog_data['listing_name'] ?></h2>
-			<button onclick="document.getElementById('edit-listing-popup').style.display='block'" class="edit-button" aria-label="profile settings">
+			<button onclick="document.getElementById('edit-listing-popup').style.display='block'" class="edit-button">
 				<i class="fas fa-edit" aria-hidden="true"></i>
 			</button>			
-      <div>
+      <div class="item">
 				<div class="item-general">
 					<div class="item-pic">
 						<img src="<?= $dog_data['listing_picture'] ?>" alt="">
@@ -34,12 +34,16 @@
 							<b>Description:&nbsp;</b> 
 							<?= $dog_data['listing_description'] ?> 
 						</p>
-						<a href="../pages/picture_change.php?id=<?=$dog_data['id']?>">Change Picture</a>
 						<?php
 							$is_author = false;
-							if ($dog_data['user_id'] === $_SESSION['id']) $is_author = true;
-							if (isset($_SESSION['id']) && !$is_author ) 
+							if (isset($_SESSION['id']) && !$is_author ) {
+								if ($dog_data['user_id'] === $_SESSION['id']) $is_author = true;
 						?> <button onclick="displayProposalPopup()"> I want to adopt it! </button>
+
+
+						<?php } else { ?>
+							<a href="../pages/picture_change.php?id=<?=$dog_data['id']?>">Change Picture</a>
+						<?php } ?>
 					</div>
 					<div class="status">
 						<table>
@@ -47,95 +51,91 @@
 								<th class="table-title" colspan="2">Dog Details</th>
 							</tr>
 							<tr>
-								<th>Gender</th>
-								<td><?= $dog_data['gender_name'] ?></td>
+								<th class="table-left">Gender</th>
+								<td class="table-right"><?= $dog_data['gender_name'] ?></td>
 							</tr>
 							<tr>
-								<th>Breed</th>
-								<td><?= $dog_data['breed_name'] ?></td>
+								<th class="table-left">Breed</th>
+								<td class="table-right"><?= $dog_data['breed_name'] ?></td>
 							</tr>
 							<tr>
-								<th>Color</th>
-								<td><?= $dog_data['color_name'] ?></td>
+								<th class="table-left">Color</th>
+								<td class="table-right"><?= $dog_data['color_name'] ?></td>
 							</tr>
 							<tr>
-								<th>Age</th>
-								<td><?= $dog_data['age_name'] ?></td>
+								<th class="table-left">Age</th>
+								<td class="table-right"><?= $dog_data['age_name'] ?></td>
 							</tr>
 						</table>
 						</div>
-					</div>
-					<div class="item-more">
-						<?php if(isset($_SESSION['id']) && $_SESSION['id'] == $dog_data['user_id']) {
-								$edit_listing = new FormCreator('edit-listing-popup', '../actions/action_edit_listing.php', true);
-								$edit_listing->add_input("listing_name", "Listing Name", "text", "New listing name", true, $dog_data['listing_name']);
-								$edit_listing->add_input("listing_description", "Listing description", "text", "New listing description", true, $dog_data['listing_description']);
-								$edit_listing->add_select('breed_id', 'Breed', get_breeds(), $dog_data['breed_id']);
-								$edit_listing->add_select('color_id', 'Color', get_colors(), $dog_data['color_id']);
-								$edit_listing->add_select('age_id', 'Age', get_ages(), $dog_data['age_id']);
-								$edit_listing->add_select('gender_id', 'Gender', get_genders(), $dog_data['gender_id']);
-								$edit_listing->add_input("dog_id", "", "hidden", "", true, $dog_data['id']);
-								$edit_listing->inline();
-							} 
-						
-							if(isset($_SESSION['id']) && $_SESSION['id'] == $dog_data['user_id']) {}
-						?>
-
-					<div>
-						<?php
-							$is_author = false;
-							if ($dog_data['user_id'] === $_SESSION['id']) $is_author = true;
-							if (isset($_SESSION['id']) && !$is_author ) { ?>
+				</div>
+				<div class="item-more">
+					<?php if(isset($_SESSION['id']) && $_SESSION['id'] == $dog_data['user_id']) {
+							$edit_listing = new FormCreator('edit-listing-popup', '../actions/action_edit_listing.php', true);
+							$edit_listing->add_input("listing_name", "Listing Name", "text", "New listing name", true, $dog_data['listing_name']);
+							$edit_listing->add_input("listing_description", "Listing description", "text", "New listing description", true, $dog_data['listing_description']);
+							$edit_listing->add_select('breed_id', 'Breed', get_breeds(), $dog_data['breed_id']);
+							$edit_listing->add_select('color_id', 'Color', get_colors(), $dog_data['color_id']);
+							$edit_listing->add_select('age_id', 'Age', get_ages(), $dog_data['age_id']);
+							$edit_listing->add_select('gender_id', 'Gender', get_genders(), $dog_data['gender_id']);
+							$edit_listing->add_input("dog_id", "", "hidden", "", true, $dog_data['id']);
+							$edit_listing->inline();
+						} 
 					
-							<?php
-								$proposal_form = new FormCreator('proposal-popup', '../actions/action_create_proposal.php', true, false);
-								$proposal_form->add_input("proposal_content", "Proposal", "text", "Write a proposal to the person!", true);
-								$proposal_form->add_input("dog_id", "", "hidden", "", true, $dog_data['id']);
-								$proposal_form->add_input("buyer_id", "", "hidden", "", true, $_SESSION['id']);
-				
-								$proposal_form->inline();
-							
-							
-								$comments = new FormCreator('comments_session', '../actions/action_make_a_question.php', false, false, false);
-								
-								$comments->add_input('question_content', 'Question', 'text', 'Write here your question', true);
-								$comments->add_input("dog_id", "", "hidden", "", true, $dog_data['id']);
-				
-								$comments->inline();
-								} 
-								
-								require_once("../database/db_class.php");
-								$dbc = Database::instance()->db();
+						// if(isset($_SESSION['id']) && $_SESSION['id'] == $dog_data['user_id']) {} 
+
+						$is_author = false;
+						if (isset($_SESSION['id']) && !$is_author) {
+							$proposal_form = new FormCreator('proposal-popup', '../actions/action_create_proposal.php', true, false);
+							$proposal_form->add_input("proposal_content", "Proposal", "text", "Write a proposal to the person!", true);
+							$proposal_form->add_input("dog_id", "", "hidden", "", true, $dog_data['id']);
+							$proposal_form->add_input("buyer_id", "", "hidden", "", true, $_SESSION['id']);
+			
+							$proposal_form->inline();
 						
-								$stmt = $dbc->prepare("SELECT * FROM comments WHERE dog_id = ?");
-								$stmt->execute(array($dog_data['id']));
-								$comments = $stmt->fetchAll();
 						
-								$num = count($comments); 
-							?>
-		
-				<br><br>
-		
-				<strong><?=$num?> Comment<?php if ($num !== 1) echo 's'; ?>:</strong>
-		
-			<?php
-				$i = 0;
-				foreach ($comments as $index => $entry) { 
-					$i++;
-						?>
-		
-					<br><br>
-					<strong>Question:</strong>
-					<?=$entry['question']?>
-					<?php $stmt = $dbc->prepare("SELECT * FROM users WHERE id = ?");
+							$comments = new FormCreator('comments_session', '../actions/action_make_a_question.php', false, false, false);
+							
+							$comments->add_input('question_content', 'Question', 'text', 'Write here your question', true);
+							$comments->add_input("dog_id", "", "hidden", "", true, $dog_data['id']);
+			
+							$comments->inline();
+						} 
+							
+							require_once("../database/db_class.php");
+							$dbc = Database::instance()->db();
+					
+							$stmt = $dbc->prepare("SELECT * FROM comments WHERE dog_id = ?");
+							$stmt->execute(array($dog_data['id']));
+							$comments = $stmt->fetchAll();
+					
+							$num = count($comments); 
+					?>
+	
+					<!-- Comments header -->
+					<strong><?=$num?> Comment<?php if ($num !== 1) echo 's'; ?>:</strong>
+	
+					<?php
+					$i = 0;
+					foreach ($comments as $index => $entry) { 
+						$i++;
+					?>
+	
+					<!-- Question -->
+					<strong>Question:</strong> <?=$entry['question']?>
+
+					<?php 
+						$stmt = $dbc->prepare("SELECT * FROM users WHERE id = ?");
 						$stmt->execute(array($entry['user_id']));
 						$user_comment = $stmt->fetch();
 					?>
-					<br> <?=$user_comment['username']?>
+					
+					<br> 
+					<!-- Username -->
+					<?=$user_comment['username']?>
 					<br>
 		
 					<?php
-					
 						if ($entry['answer'] === NULL && $is_author) {
 		
 							$add_answer = new FormCreator('answer', '../actions/action_write_a_answer.php', false, false, false);
@@ -148,18 +148,16 @@
 		
 						}
 						else { ?>
-		
-							<strong>Answer:</strong>
-							<?=$entry['answer']?>
-						<?php	
-						}
-		
-				} ?>
-		
-				</div>
-      </div>
-		</div>
-		</div>
+						<!-- Answer -->
+						<strong>Answer:</strong> <?=$entry['answer']?> 
+					<?php	
+						}				
+					}
+					?>
+
+				</div> <!-- End item-more  -->
+			</div> <!-- End item  -->
+		</div> <!-- End main70 -->
 	
 		<div class="right15">
 		</div>
