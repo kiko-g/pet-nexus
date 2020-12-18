e95413a1b5eb5f206f70087bb590ccae6b80fbbe
Jose Silva <callfodutypsp@gmail.com>
2020-12-18 02:34:47 +0000

    CSRF for deny proposal

--- actions/action_deny_proposal.php
+++ actions/action_deny_proposal.php
@@ -3,7 +3,7 @@
 include_once('../includes/session.php');
 include_once('../database/dogs.php');
 
-answer_proposal($_GET['id'], 2);
+answer_proposal($_GET, 2);
 
 header('Location: ../pages/proposals.php');
 
