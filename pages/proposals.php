6e06f8bd4dbca1fac8ec2362f5d96563bda8727b
Jose Silva <callfodutypsp@gmail.com>
2020-12-20 18:28:02 +0000

    More details on proposals page

--- pages/proposals.php
+++ pages/proposals.php
@@ -100,7 +100,6 @@ $username = $stmt->fetch()['username'];
 			<article id="incoming-proposals" class="proposals">
 				<h2 class="center">Incoming Proposals</h2>
 				<?php 
-					require_once("../database/db_class.php");
 					$dbc = Database::instance()->db();
 					$stmt = $dbc->prepare("SELECT proposals.id, proposal_text, buyer_id, users.username as buyer_username, listing_picture, listing_name, dog_id
 						FROM proposals 
@@ -135,15 +134,14 @@ $username = $stmt->fetch()['username'];
 			<article id="outgoing-proposals" class="proposals">
 				<h2 class="center">Outgoing Proposals</h2>
 				<?php 
-					require_once("../database/db_class.php");
 					$dbc = Database::instance()->db();
-					$stmt = $dbc->prepare("SELECT proposals.id, proposal_text, buyer_id, users.username as buyer_username, listing_picture, listing_name, dog_id
+					$stmt = $dbc->prepare("SELECT proposals.id, proposal_text, buyer_id, users.username as seller_username, seller_id, listing_picture, listing_name, dog_id
 						FROM proposals 
 						JOIN users 
-						ON buyer_id = users.id
+						ON seller_id = users.id
 						JOIN dogs
 						ON dog_id = dogs.id
-						WHERE seller_id = ? AND proposal_status = 0");
+						WHERE buyer_id = ? AND proposal_status = 0");
 					$stmt->execute(array($_SESSION['id']));
 					$proposals = $stmt->fetchAll(); 
 
@@ -154,12 +152,10 @@ $username = $stmt->fetch()['username'];
 					<div class="proposal-main">
 						<img src="<?=$entry['listing_picture']?>" alt="">
 						<div></div>
-						<button class="yes" onclick="accept_proposal(<?=$entry['id']?>)">Yes <i class="fas fa-check" aria-hidden="true"></i></button>
-						<button class="no" onclick="deny_proposal(<?=$entry['id']?>)">No <i class="fas fa-times" aria-hidden="true"></i></button>
 					</div>
 					<div class="proposal-description">
 						<p><strong>Dog name</strong>: <a href="item.php?id=<?= $entry['dog_id'] ?>"><?=$entry['listing_name']?></a></p>
-						<p><strong>Proposal from</strong>: <a href="profile.php?id=<?=$entry['buyer_id']?>"><?=$entry['buyer_username'];?></a></p>
+						<p><strong>Proposal to</strong>: <a href="profile.php?id=<?= $entry['seller_id'] ?>"><?=$entry['seller_username']?></a></p>
 						<p><strong>Proposal description</strong>: <?=$entry['proposal_text']?></p>
 					</div>
 				</figure>
@@ -170,15 +166,14 @@ $username = $stmt->fetch()['username'];
 			<article id="previous-proposals" class="proposals">
 				<h2 class="center">Previous Proposals</h2>
 				<?php 
-					require_once("../database/db_class.php");
 					$dbc = Database::instance()->db();
-					$stmt = $dbc->prepare("SELECT proposals.id, proposal_text, buyer_id, users.username as buyer_username, listing_picture, listing_name, dog_id
+					$stmt = $dbc->prepare("SELECT proposals.id, proposal_text, buyer_id, users.username as seller_username, seller_id, listing_picture, listing_name, dog_id, proposal_status
 						FROM proposals 
 						JOIN users 
-						ON buyer_id = users.id
+						ON seller_id = users.id
 						JOIN dogs
 						ON dog_id = dogs.id
-						WHERE seller_id = ? AND proposal_status = 0");
+						WHERE buyer_id = ? AND proposal_status in (1,2)");
 					$stmt->execute(array($_SESSION['id']));
 					$proposals = $stmt->fetchAll(); 
 
@@ -189,13 +184,12 @@ $username = $stmt->fetch()['username'];
 					<div class="proposal-main">
 						<img src="<?=$entry['listing_picture']?>" alt="">
 						<div></div>
-						<button class="yes" onclick="accept_proposal(<?=$entry['id']?>)">Yes <i class="fas fa-check" aria-hidden="true"></i></button>
-						<button class="no" onclick="deny_proposal(<?=$entry['id']?>)">No <i class="fas fa-times" aria-hidden="true"></i></button>
 					</div>
 					<div class="proposal-description">
 						<p><strong>Dog name</strong>: <a href="item.php?id=<?= $entry['dog_id'] ?>"><?=$entry['listing_name']?></a></p>
-						<p><strong>Proposal from</strong>: <a href="profile.php?id=<?=$entry['buyer_id']?>"><?=$entry['buyer_username'];?></a></p>
 						<p><strong>Proposal description</strong>: <?=$entry['proposal_text']?></p>
+						<p><strong>Proposal to</strong>: <a href="profile.php?id=<?= $entry['seller_id'] ?>"><?=$entry['seller_username']?></a></p>
+						<p><strong>Status: </strong><?= $entry['proposal_status'] == '1' ? 'Accepted' : 'Rejected'; ?></p>
 					</div>
 				</figure>
 
